17df5c69873c0480dcc0c4a237f2452547926994
Gabe Finch and Lizzie Koehler <gabefinch@gmail.com>
2015-02-18 15:07:50 -0800

    add price calculation to the page

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ Ticket.adjustPrice = function() {
   } else if (this.age === 'Student') {
     this.price = this.price - 1.25;
   }
-  if (this.time < Date.parse('01/01/2015 17:00:00')){
+  if (this.time < Date.parse('01/01/2015 5:00:00')){
     this.price = this.price - 2.75;
   }
   if (!this.newRelease){
@@ -16,31 +16,51 @@ Ticket.adjustPrice = function() {
 };
 
 $(function() {
+  $(".time li").click( function(event){
+    event.preventDefault();
+    var time = $(event.target).text()
+    var newTicket = Object.create(Ticket);
+    newTicket.time = Date.parse('01/01/2015 ' + time + ':00')
+    newTicket.age = $('#age').val();
+    newTicket.adjustPrice();
+    $('#ticketInfo').empty();
+    $('#ticketInfo').append('<h4>The ' + newTicket.age + ' Price for the ' + time + ' Showing is: $' + newTicket.price + '</h4>');
+
+  });
+
   $("#hotPursuit").hover(
     function() {
+      $('#banner').empty().append("Now Playing");
       $('span.poster').html('<img src="css/img/hotpursuit.jpg">');
     }, function() {
+      $('#banner').empty().append("Coming Soon!");
       $('span.poster').html('<img src="css/img/starwars.jpg">');
     }
   );
   $("#breakfastClub").hover(
     function() {
+      $('#banner').empty().append("Now Playing");
       $('span.poster').html('<img src="css/img/breakfastclub.jpg">');
     }, function () {
+      $('#banner').empty().append("Coming Soon!");
       $('span.poster').html('<img src="css/img/starwars.jpg">');
     }
   );
   $("#barbarella").hover(
     function() {
+      $('#banner').empty().append("Now Playing");
       $('span.poster').html('<img src="css/img/barbarella.jpg">');
     }, function () {
+      $('#banner').empty().append("Coming Soon!");
       $('span.poster').html('<img src="css/img/starwars.jpg">');
     }
   );
   $("#laStrada").hover(
     function() {
+      $('#banner').empty().append("Now Playing");
       $('span.poster').html('<img src="css/img/lastrada.jpg">');
     }, function () {
+      $('#banner').empty().append("Coming Soon!");
       $('span.poster').html('<img src="css/img/starwars.jpg">');
     }
   );
